ed3d1b283caf87e671dc6ac112e875a6ed4a9cb0
özgü <oezgue.dogan@gmail.com>
2012-06-28 17:29:37 +0200

    Vorbereitung abgeschlossen

--- PR MDV - Termin 7 und 8/Matlab/Hauptprogramm.m
+++ PR MDV - Termin 7 und 8/Matlab/Hauptprogramm.m
@@ -3,8 +3,23 @@
 t = 0:0.001:2;
 u = chirp(t,0,1,100);
 
-frequenz_imZeitbereich_ausSignal(u,t)
+figure(1);
+plot(u)
+xlabel('Zeitachse [ms]');
+ylabel('Amplitude [V]');
+title('\bf Chirp-Signal über der Zeit');
+AXIS([0 2000 -1.2 1.2]);
+
 figure(2);
 spectrogram(u,256,250,256,1E3)
+title('\bf Spektrogramm des Chirp-Signals');
+figure(3);
+spectrogram(u,256,250,100,1E3)      %kleinere Überlappung zw Segmenten
+title('\bf Spektrogramm mit kleinerer Überlappungsläche');
+figure(4);
+spectrogram(u,500,250,256,1E3)      %größeres Fenter
+title('\bf Spektrogramm mit größerem Fenster');
 
-frequenz_durch_Spektogramm(u,t)
+
+% frequenz_imZeitbereich_ausSignal(u,t)
+% frequenz_durch_Spektogramm(u,t)
