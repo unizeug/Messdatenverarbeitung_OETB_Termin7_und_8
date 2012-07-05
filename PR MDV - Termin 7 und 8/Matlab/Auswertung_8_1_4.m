a51191c814c5ff80faf590df02360b90f6feae1f
özgü <oezgue.dogan@gmail.com>
2012-07-05 13:59:00 +0200

    Matlab

---
+++ PR MDV - Termin 7 und 8/Matlab/Auswertung_8_1_4.m
@@ -0,0 +1,37 @@
+%MDV Praktikum 7 Vorbereitungsaufgabe
+%Auswertung Aufgabe 4
+
+close all; clc, clear;
+
+load('strom');
+
+S = haardeclevel_8_1_2(x,5);
+
+
+fignum=801;
+for lvl=1:5
+    [u v] = getAppDet_8_1_3(S, lvl);
+    N=length(u);
+    u2=ones(1,N*2^lvl);
+    v2=ones(1,N*2^lvl);
+    j=1;
+%     Die Approximation und die Details auf die Länge von x normieren
+    for i=1:N
+        for k=1:2^lvl
+            u2(j) = u(i);
+            v2(j) = v(i);
+            j = j+1;
+        end
+    end
+
+    figure(fignum);
+    hold on
+        plot(x)
+        plot(u2,'r')
+        plot(v2,'c')
+    hold off
+
+    fignum= fignum+1;
+    
+end
+
